4e1d55e45d16f899cbf59884dc6ec3e840e9b985
Gilson Santos <gilsonjosert@gmail.com>
2021-09-18 21:52:30 +0000

    tab-horizontal script success!

--- src/pages/Details/script.js
+++ src/pages/Details/script.js
@@ -1,10 +1,28 @@
 // const js = document.createElement("script").setAttribute('src',"../../scripts/index.js");
 // document.querySelector("head").appendChild(js);
 
-const buttonList = document.querySelector('section>.lead-actions>button:nth-child(1)');
+const links = document.querySelectorAll('.tab-horizontal>.tab-head>a');
+const contents = document.querySelectorAll('.tab-horizontal>.tab-body>.tab-content');
 
-buttonList.addEventListener('click',(_)=>{
-    const list = document.querySelector('section.list-products');
-    list.classList.toggle('list');
-    buttonList.classList.toggle('grid');
+links.forEach((item)=>{
+    item.addEventListener('click',()=>{
+        this.handlessTabView(item);
+    });
 });
+
+function handlessTabView(item){
+    contents.forEach((content)=>{
+        if(item.dataset.id == content.id){
+            item.classList.add('active');
+            content.classList.add('active');
+        }else{
+            content.classList.remove('active');
+            links.forEach((it)=>{
+                if(item.dataset.id != it.dataset.id){
+                    it.classList.remove('active');
+                }
+            });
+            
+        }
+    });
+}
