c1ac5ddc84aab847577a81bc7a20700a7161bb66
Gilson Santos <gilsonjosert@gmail.com>
2021-09-21 13:14:45 +0000

    Script swap img product

--- src/pages/ListagemCategoria/script.js
+++ src/pages/ListagemCategoria/script.js
@@ -1,5 +1,7 @@
 // const js = document.createElement("script").setAttribute('src',"../../scripts/index.js");
 // document.querySelector("head").appendChild(js);
+window.onload = function(){
+
 
 const buttonList = document.querySelector('section>.lead-actions>button:nth-child(1)');
 
@@ -8,3 +10,21 @@ buttonList.addEventListener('click',(_)=>{
     list.classList.toggle('list');
     buttonList.classList.toggle('grid');
 });
+
+const imgGaleria = document.querySelectorAll('.galeria>span');
+
+function swap(src){
+    console.log('aqui!');
+    if(src != undefined){
+        let prevImg = document.querySelector('.picture>img');
+        console.log(prevImg.currentSrc,src);
+    }
+}
+
+imgGaleria.forEach(item=>{
+    console.log(item);
+    item.addEventListener('click',swap.bind(this));
+})
+
+};
+
