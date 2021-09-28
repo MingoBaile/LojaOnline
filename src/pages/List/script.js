06e7f2f4b805bcbf801af0c9bbc3c8f5d4bd3069
Gilson Santos <gilsonjosert@gmail.com>
2021-09-28 23:02:59 +0000

    script link

--- src/pages/List/script.js
+++ src/pages/List/script.js
@@ -26,6 +26,6 @@ window.onload = function(){
     const btnHome = document.querySelector('.btnHome');
 
     btnHome.addEventListener('click',()=>{
-        document.location = 'https://5500-maroon-marsupial-yki2sr1z.ws-us18.gitpod.io/src/pages/Home/index.html';
+        document.location = '../Home/index.html';
     })
 }
